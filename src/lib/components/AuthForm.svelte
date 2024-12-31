cce4e69508f9b77253a734f6c1eb5b813e5b0629
VinayN03 <vinaykumar.namadi@gmail.com>
2024-12-30 22:19:54 -0600

    Home Page and Register Page

---
+++ src/lib/components/AuthForm.svelte
@@ -0,0 +1,109 @@
+<script lang="ts">
+	import type { ActionData } from "../../routes/register/$types";
+	import Button from "./Button.svelte";
+
+    interface ComponentProps {
+        isRegistration: boolean;
+        form: ActionData;
+    }
+
+    let { isRegistration, form }: ComponentProps = $props();
+</script>
+
+<div class="default-margin auth-container">
+    <h2 class="mb-l"> {isRegistration ? "Register" : "Login" }</h2>
+    <div class="form-and-social-login">
+        <form class="auth-form" method="POST">
+
+            {#if form && form.errors?.length}
+            {#each form.errors as error}
+            <div class="auth-error">
+            <p>{error}</p>
+        </div>
+        {/each}
+            <!-- <h3>{form.message}</h3> -->
+            {/if}
+
+            {#if isRegistration}
+                <input placeholder="Name" type="text" name="name" />
+            {/if}
+            <input placeholder="Email" type="text" name="email" />
+            <input placeholder="Password" type="password" name="password" />
+            {#if isRegistration}
+            <input
+                placeholder="Confirm Password"
+                type="password"
+                name="passwordConfirmation" 
+            />
+            {/if}
+            <Button type="submit"> {isRegistration ? "Register" : "Login"}</Button>
+
+            {#if isRegistration}
+            <p class="auth-hint mt-s">
+                Already have an Account? <a href="/login">Log In.</a>
+            </p>
+            {:else}
+            <p class="auth-hint mt-s">
+                Do not have an account yet? <a href="/register">Sign Up</a>
+            </p>
+            {/if}
+
+
+        </form>
+        <div class="social-login">
+            <!-- Add a button here -->
+        </div>
+    </div>
+</div>
+
+<style>
+    .auth-container {
+        margin-top: 10px;
+    }
+
+    .form-and-social-login {
+        display: flex;
+    }
+
+    .auth-form {
+        display: flex;
+        flex-direction: column;
+        align-items: start;
+        border-right: 1px solid grey;
+        padding-right: 80px;
+        width: 40%;
+    }
+
+    .auth-hint {
+        font-size: 16px;
+        color: grey;
+    }
+
+    .auth-error {
+        background-color: rgb(122, 35, 35);
+        color: white;
+        font-size: 12px;
+        border-radius: 12px;
+        padding: 12px;
+        width: 100%;
+        margin-bottom: 8px;
+    }
+
+    .auth-error:last-of-type {
+        margin-bottom: 16px;
+
+    }
+    .auth-form input {
+        width: 100%;
+        margin-bottom: 12px;
+    }
+
+    .auth-form input:last-of-type {
+        margin-bottom: 30px;
+    }
+    .social-login {
+        padding-left: 80px;
+        width: 40%;
+    }
+
+</style>
