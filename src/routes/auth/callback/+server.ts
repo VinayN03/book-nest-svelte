cea05cdf4c08976c6c335a06cf042f351854e4b2
VinayN03 <vinaykumar.namadi@gmail.com>
2025-01-01 13:20:06 -0600

    Database setup and addition of username on registration

--- src/routes/auth/callback/+server.ts
+++ src/routes/auth/callback/+server.ts
@@ -10,6 +10,26 @@ export const GET: RequestHandler = async ({ url, locals: { supabase } }) => {
     const sessionData = await supabase.auth.getSession();
 
     if (sessionData.data.session) {
+
+        const userId = sessionData.data.session.user.id;
+        const userName = sessionData.data.session.user.user_metadata.name;
+
+        const { data: existingUser, error: selectError } = await supabase.from("user_names").select('name').eq('user_id', userId).single()
+
+        if (selectError && selectError.code != 'PGRST116') {
+            return new Response("Failed to check for existing user", { status: 500});
+        }
+
+        if (!existingUser) {
+            const { error: insertError } = await supabase.from("user_names").insert([{ user_id: userId, name: userName}])
+        
+        if (insertError) {
+            return new Response("Failed to insert user name", { status: 500 });
+        }
+        }
+
+        // console.log(sessionData.data.session.user);
+        
         throw redirect(303, "/private/dashboard");
     }
 
