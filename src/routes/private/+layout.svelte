e9ab95c35c66bc58e9a7964d4a8b0eed8670341e
VinayN03 <vinaykumar.namadi@gmail.com>
2025-01-01 22:44:44 -0600

    User Dashboard

--- src/routes/private/+layout.svelte
+++ src/routes/private/+layout.svelte
@@ -10,3 +10,18 @@
     </div>
 
 </div>
+
+<style>
+    .auth-area {
+        display: flex;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    .main-area {
+        flex: 1;
+        min-width: 0;
+        padding: 40px 4vw 100px 40px;
+        overflow-y: auto;
+    }
+</style>
--- src/routes/private/dashboard/+page.svelte
+++ src/routes/private/dashboard/+page.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
 	import { getUserState } from "$lib/state/user-state.svelte";
+	import Icon from "@iconify/svelte";
 
 
     let userContext = getUserState();
-    let {allBooks} = $derived(userContext);
+    let { userName } = $derived(userContext);
 
-    $inspect(allBooks);
     
 
 </script>
 
-<ul>
-    {#each allBooks as book}
-    <li>{book.title}</li>
-    {/each}
-</ul>
+<div class="dashboard">
+    <div class="dashboard-header mb-m">
+        <a href="/private/scan-shelf" class="add-book">
+        <Icon icon="icons8:plus" width={"72"} height={"72"} />
+        <p>Add a book</p>
+        </a>
+        <div class="headline">
+            <h3 class="bold mb-xs">Welcome Back, {userName}</h3>
+            <p>There's nothing quite like the journey a good book can take you on. Have you 
+                discovered any new favourites recently?
+            </p>
+        </div>
+    </div>
+</div>
